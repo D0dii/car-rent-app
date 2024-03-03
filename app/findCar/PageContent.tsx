7c24226d7209d50d1fb6f20aec6df8b70ce5ab52
Marcin Dolatowski <106433931+D0dii@users.noreply.github.com>
2024-03-03 23:30:18 +0100

    Adding react hook form. finishing extracting data from form on find car page

--- app/findCar/PageContent.tsx
+++ app/findCar/PageContent.tsx
@@ -14,11 +14,43 @@ import { TimePicker } from "@mui/x-date-pickers/TimePicker";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import dayjs from "dayjs";
+
+type FormValues = {
+  pickupLocation: string;
+  dropoffLocation?: string;
+  pickupDate: Date;
+  pickupTime: Date;
+  dropoffDate: Date;
+  dropoffTime: Date;
+};
+
 export default function PageContent({ data }: { data: any }) {
   const [dropOffDifferentLocation, setDropOffDifferentLocation] = useState(false);
   function handleDropOffDifferentLocation() {
     setDropOffDifferentLocation(!dropOffDifferentLocation);
   }
+  function createNewDatePlusThreeDays() {
+    let date = new Date();
+    date.setDate(date.getDate() + 3);
+    return date;
+  }
+  const form = useForm<FormValues>({
+    defaultValues: {
+      pickupLocation: "",
+      dropoffLocation: "",
+      pickupDate: new Date(),
+      pickupTime: new Date("December 17, 1995 10:00:00"),
+      dropoffDate: createNewDatePlusThreeDays(),
+      dropoffTime: new Date("December 17, 1995 10:00:00"),
+    },
+  });
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
   return (
     <Container>
       <Box
@@ -30,7 +62,7 @@ export default function PageContent({ data }: { data: any }) {
       >
         <Typography variant="h3">Car Rental App - Search, Compare and Save</Typography>
       </Box>
-      <form action="">
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Box
           display={"flex"}
           alignItems={"center"}
@@ -51,31 +83,38 @@ export default function PageContent({ data }: { data: any }) {
             }}
           >
             <Autocomplete
+              isOptionEqualToValue={(option: { label: string }, value: { label: string }) =>
+                option.label === value.label
+              }
               disablePortal
               options={data}
               sx={{ width: 300 }}
               renderInput={(params) => (
                 <TextField
+                  {...register("pickupLocation", { required: "Pickup location is required" })}
                   {...params}
                   label="Pickup Location"
-                  id="pickupLocation"
-                  name="pickupLocation"
-                  required
+                  error={!!errors.pickupLocation}
+                  helperText={errors.pickupLocation?.message}
                 />
               )}
             ></Autocomplete>
 
             {dropOffDifferentLocation ? (
               <Autocomplete
+                isOptionEqualToValue={(option: { label: string }, value: { label: string }) =>
+                  option.label === value.label
+                }
                 disablePortal
                 options={data}
                 sx={{ width: 300 }}
                 renderInput={(params) => (
                   <TextField
+                    {...register("dropoffLocation", { required: "Dropoff location is required" })}
                     {...params}
                     label="Dropoff Location"
-                    id="dropoffLocation"
-                    name="dropoffLocation"
+                    error={!!errors.dropoffLocation}
+                    helperText={errors.dropoffLocation?.message}
                   />
                 )}
               ></Autocomplete>
@@ -98,8 +137,41 @@ export default function PageContent({ data }: { data: any }) {
                 },
               }}
             >
-              <DatePicker label="Pick-up date" sx={{ width: 300 }} />
-              <TimePicker label="Time" sx={{ width: 300 }} />
+              <Controller
+                control={control}
+                name="pickupDate"
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <DatePicker
+                      label="Pick-up date"
+                      onChange={(date) => {
+                        field.onChange(date);
+                      }}
+                      value={dayjs(field.value)}
+                      sx={{ width: 300 }}
+                    />
+                  );
+                }}
+              />
+              <Controller
+                control={control}
+                name="pickupTime"
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <TimePicker
+                      label="Time"
+                      onChange={(date) => {
+                        field.onChange(date);
+                      }}
+                      value={dayjs(field.value)}
+                      sx={{ width: 300 }}
+                    />
+                  );
+                }}
+              />
+              {/* <DatePicker label="Pick-up date" sx={{ width: 300 }} /> */}
             </Box>
             <Box
               display={"flex"}
@@ -111,8 +183,42 @@ export default function PageContent({ data }: { data: any }) {
                 },
               }}
             >
-              <DatePicker label="Drop-off date" sx={{ width: 300 }} />
-              <TimePicker label="Time" sx={{ width: 300 }} />
+              {/* <DatePicker label="Drop-off date" sx={{ width: 300 }} />
+              <TimePicker label="Time" sx={{ width: 300 }} /> */}
+              <Controller
+                control={control}
+                name="dropoffDate"
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <DatePicker
+                      label="Drop-off date"
+                      onChange={(date) => {
+                        field.onChange(date);
+                      }}
+                      value={dayjs(field.value)}
+                      sx={{ width: 300 }}
+                    />
+                  );
+                }}
+              />
+              <Controller
+                control={control}
+                name="dropoffTime"
+                rules={{ required: true }}
+                render={({ field }) => {
+                  return (
+                    <TimePicker
+                      label="Time"
+                      onChange={(date) => {
+                        field.onChange(date);
+                      }}
+                      value={dayjs(field.value)}
+                      sx={{ width: 300 }}
+                    />
+                  );
+                }}
+              />
             </Box>
           </LocalizationProvider>
           <Box mt={"1rem"}>
