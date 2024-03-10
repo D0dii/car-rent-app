657adaefd346dd213ae75a4dd5c97ac3d7f588e2
Marcin Dolatowski <106433931+D0dii@users.noreply.github.com>
2024-03-10 21:43:55 +0100

    Starting working on making rental

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,3 +13,25 @@ export const getCars = async (pickupLocation: string) => {
 
   return { props: { cars } };
 };
+
+export function bookCar(
+  carId: string,
+  userId: string,
+  pickupLongitude: string,
+  pickupLatitude: string,
+  dropoffLongitude: string,
+  dropoffLatitude: string,
+  startDate: Date,
+  endDate: Date,
+  totalCost: number,
+  securityDeposit: number
+) {
+  return prisma.car.update({
+    where: {
+      id: carId,
+    },
+    data: {
+      isAvailable: false,
+    },
+  });
+}
